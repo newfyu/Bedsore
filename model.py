d7565193e0f86cbd5ef07ee5108694001c430462
newfyu <lhan12345@hotmail.com>
2021-05-23 21:07:21 +0800

    fix some bug

--- model.py
+++ model.py
@@ -18,6 +18,7 @@ from torchvision.models.detection.anchor_utils import AnchorGenerator
 from efficientnet_pytorch import EfficientNet
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+
 class EfficientNetBackBone(torch.nn.Module):
 
     def __init__(self, model_name):
@@ -75,7 +76,7 @@ class MyFasterRCNN(pl.LightningModule):
         loss = sum(loss for loss in loss_dict.values())
         #  result = pl.TrainResult(minimize=loss)
         #  result.log_dict({"train_loss": loss}, prog_bar=True)
-        self.log('train_loss',loss, prog_bar=True)
+        self.log('train_loss', loss, prog_bar=True)
         return loss
 
     def cal_ap(self, outputs):
@@ -102,15 +103,6 @@ class MyFasterRCNN(pl.LightningModule):
             mAP.append(ap)
         mAP = sum(mAP) / len(mAP)
         return mAP, class_ap
-
-    #  def validation_step(self, batch, batch_idx):
-        #  self.net.train()  # net output loss when set train
-        #  images, targets = batch
-        #  loss_dict = self(images, targets)
-        #  loss = sum(loss for loss in loss_dict.values())
-        #  result = pl.EvalResult(checkpoint_on=loss)
-        #  result.log_dict({'loss/valid_loss': loss}, prog_bar=True)
-        #  return result
 
     @torch.no_grad()
     def validation_step(self, batch, batch_idx):
@@ -140,12 +132,12 @@ class MyFasterRCNN(pl.LightningModule):
         mAP, class_ap = self.cal_ap(outputs)
         print(class_ap)
         mAP, class_ap = self.cal_ap(outputs)
-        self.log("valid_map", mAP, prog_bar=True)
+        self.log("test_mAP", mAP, prog_bar=True)
         return mAP
 
     def configure_optimizers(self):
-        #  optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.lr, momentum=0.9)
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        optimizer = torch.optim.SGD(self.parameters(), lr=self.hparams.lr, momentum=0.9)
+        #  optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
         return optimizer
 
     #  def configure_optimizers(self):
@@ -196,6 +188,7 @@ def main():
                                chunk_id=args.chunk_id,
                                num_workers=args.num_workers,
                                seed=args.seed)
+    #  trainer = Trainer.from_argparse_args(args, auto_lr_find=True)
     trainer = Trainer.from_argparse_args(args)
 
     if args.test_ckpt != "":  # test
@@ -213,8 +206,9 @@ def main():
         else:
             logger = None
         trainer.logger = logger
-        checkpoint_callback = ModelCheckpoint(monitor='valid_map',save_last=True,mode='max')
+        checkpoint_callback = ModelCheckpoint(monitor='valid_map', save_last=True, mode='max')
         trainer.callbacks.append(checkpoint_callback)
+        #  trainer.tune(model, dm)
         trainer.fit(model, dm)
         trainer.test()
 
